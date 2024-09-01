eaf2d308af2e447d7a029333bb85fc8420ba7023
Padmalochan6372 <padmalochanmahanta506@gmail.com>
2024-09-01 22:36:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7497264274:AAEjzv05xuy675MMuDZE474rtGF0_u3IqC8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@Allmovieavailable37")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1081561018")
 
 
 
